c76e9c57f0a1fd50bdbf19af2c41befc963e22af
FITA . <nacholopezamat@gmail.com>
2025-10-01 14:13:09 +0200

    commit

--- index.ts
+++ index.ts
@@ -1,2 +1,59 @@
-console.log("Hola munde")
 
+type Lugar ={
+    id:number,
+    nombre:string,
+    tipo:string,
+    peligro:number
+}
+
+type Personaje={
+    id:number,
+    nombre:string,
+    fuerza:number,
+    aliado:boolean
+}
+type Objeto={
+    id:number,
+    nombre:string,
+    poder:number,
+    categoria:string
+}
+
+const lugares: Lugar[] = [
+  { id: 1, nombre: "Bosque Misterioso", tipo: "bosque", peligro: 2 },
+  { id: 2, nombre: "Cueva Oscura", tipo: "cueva", peligro: 5 },
+  { id: 3, nombre: "Castillo Abandonado", tipo: "castillo", peligro: 4 }
+];
+ 
+const personajes: Personaje[] = [
+  { id: 1, nombre: "Guardián", fuerza: 8, aliado: false },
+  { id: 2, nombre: "Sabio", fuerza: 2, aliado: true },
+  { id: 3, nombre: "Bandido", fuerza: 6, aliado: false }
+];
+ 
+const objetos: Objeto[] = [
+  { id: 1, nombre: "Espada", poder: 5, categoria: "arma" },
+  { id: 2, nombre: "Antorcha", poder: 1, categoria: "herramienta" },
+  { id: 3, nombre: "Amuleto", poder: 3, categoria: "mágico" },
+  { id: 4, nombre: "Escudo", poder: 4, categoria: "arma" }
+];
+
+
+
+
+const listarLugares=()=>{
+    lugares.forEach((l)=>{
+        console.log(l.nombre+", peligro:"+ l.peligro)
+    })
+}
+
+const buscarPersonaje=(nombre:string)=>{
+const resultado = personajes.filter((p) => {
+  if (p.nombre === nombre) {
+   console.log(`Encontrado: ${p.nombre}, fuerza: ${p.fuerza}, ${p.aliado ? "Aliado" : "Enemigo"}`
+  );
+    return true; 
+  }
+  return false;
+});
+}
